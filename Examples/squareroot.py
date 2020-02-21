8bacf4523d8e02d71dd3918bdbe939e13b442cd6
Paakofi72 <koo7@njit.edu>
2020-02-20 19:51:21 -0500

    Feature:Added an abstraction example

---
+++ Examples/squareroot.py
@@ -0,0 +1,12 @@
+class SquareRoot:
+
+    @staticmethod
+    def squareroot(radicand, degree):
+        return (radicand ** (1 / degree))
+
+
+import math
+math.sqrt(9)
+'this is an example of abstraction because instead of making the SquareRoot function, you call just import the '
+'function and show only the relevant operations like the math.sqrt() operation'
+
---
+++ Tests/Exampe_test.py
@@ -0,0 +1,12 @@
+import unittest
+from Examples.squareroot import SquareRoot
+
+class MyTestCase(unittest.TestCase):
+    def test_Examples_root(self):
+        self.assertEqual(6, SquareRoot.squareroot(36, 2))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
